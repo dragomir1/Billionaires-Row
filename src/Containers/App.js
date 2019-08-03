da4a71f516ac1d0f7e5fb35a6f0d75b24e166f8f
dragomir1 <16617178+dragomir1@users.noreply.github.com>
2019-08-03 12:44:16 -0700

    created and added logto to App.js

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import '../configuration/firebase.js';
-import './App.css';
+import '../App.css';
+import logo from '../assets/images/logo.png';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <h1>Fortune favors the bold...</h1>
-        <p>
-          Welcome to Billionaires' Row. The Idle game where you grow your wealth
-          and own the world.
-        </p>
+        <p> Build your wealth and own the world!</p>
+        <img src={logo} className='App-logo' alt='logo' />
       </header>
     </div>
   );
